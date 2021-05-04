c7c2acecd294ae832a8ae0e39600e9ee92385cb3
mikegoolsby <55331311+mikegoolsby@users.noreply.github.com>
2021-05-04 13:44:21 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import time
+
+print('Press ENTER to begin. Afterward, press ENTER to "click" the stopwatch. Press Ctrl-C to quit.')
+input() #Press enter to begin
+print('Started.')
+start_time = time.time() # get first lap start time
+last_time = start_time
+lap_num = 1
+
+
+while True:
+    input()
+    lap_time = round(time.time() - last_time, 2)
+    total_time = round(time.time() - start_time, 2)
+    print('Lap #%s: %s (%s)' % (lap_num, total_time, lap_time), end='')
+    lap_num+=1
+    last_time = time.time() # reset last lap time
